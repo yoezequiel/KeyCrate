95002f9b0ef51eef8d2353dc92b679a0ee72cab0
yoezequiel <ezequielcasiano15@gmail.com>
2023-08-02 12:24:26 -0300

    test DB and new ENV

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/yoezequiel/Programming/Django/test-web/env"
+set -gx VIRTUAL_ENV "/home/yoezequiel/Programming/keycrate/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
